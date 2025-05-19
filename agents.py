cbb6e1ad3d279321e3424b8281df16eb4852a632
florinhegedus <florinxhgds@gmail.com>
2025-05-19 22:51:36 +0300

    Update BasicAgent to read in file path

--- agents.py
+++ agents.py
@@ -5,4 +5,16 @@ class BasicAgent:
         print(f"Agent received question (first 50 chars): {question[:50]}...")
         fixed_answer = "This is a default answer."
         print(f"Agent returning fixed answer: {fixed_answer}")
+
+        # The decision what tool to use for each file will be taken by the agent
+        if file_path is not None:
+            if file_path[-4:] == ".mp3":
+                print("Processing audio file")
+            elif file_path[-5:] == ".xlsx":
+                print("Processing excel file")
+            elif file_path[-3:] == ".py":
+                print("Processing python file")
+            elif file_path[-4:] == ".png":
+                print("Processing image file")
+    
         return fixed_answer
